0d53d9cff5722f5ec7e6c3293b742222db1ed7b9
Leonardo Sasseron <leo.dias360@gmail.com>
2019-04-08 10:26:59 -0300

    Inimigo

--- Prot/Assets/Scripts/Enemy.cs
+++ Prot/Assets/Scripts/Enemy.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private PlayerController player;
+    private PlayerController player;    
 
-     void Start()
+    void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();        
     }
 
      void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
-        {
+        {            
             player.Damage(1);
-        }
+        }        
 
         //ienumerator playercontroller
         //StartCoroutine(player.Knockback(0.02f, 350, player.transform.position));
-    }
+    }    
 }
---
+++ Prot/Assets/Scripts/Enemy2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    private PlayerController player;
+    public GameObject bullet;    
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();        
+    }
+
+     void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {            
+            StartCoroutine(Attack());
+            //player.Damage(1);
+        }     
+
+        //ienumerator playercontroller
+        //StartCoroutine(player.Knockback(0.02f, 350, player.transform.position));
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            StopAllCoroutines();
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(1);
+        Instantiate(bullet, transform.position, transform.rotation);
+        StartCoroutine(Attack());
+    }  
+}
